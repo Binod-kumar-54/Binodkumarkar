6d55da23f2c222943b788eebb44a29f7c5b69b0c
Binod Kumar <139119391+Binod-kumar-54@users.noreply.github.com>
2023-07-30 14:53:00 +0530

    Add files via upload

---
+++ birthday/cd.js
@@ -0,0 +1,31 @@
+const endDate = "15 january 2024 4:08 AM"
+
+document.getElementById('end-date').innerText = endDate;
+const inputs = document.querySelectorAll("input");
+
+//const clock = () => {
+
+//}
+function clock(){
+    const end = new Date(endDate);
+    const now = new Date();
+    const diff = (end-now)/1000;
+
+    if (diff < 0) return;
+    // convert to days ;
+    inputs[0].value = (Math.floor(diff/3600/24));
+    inputs[1].value = (Math.floor(diff/3600)%24);
+    inputs[2].value = (Math.floor(diff/60)%60);
+    inputs[3].value = (Math.floor(diff)%60);
+
+
+}
+//initial clock
+clock()
+
+setInterval(
+    () => {
+        clock()
+    },
+    1000
+)
